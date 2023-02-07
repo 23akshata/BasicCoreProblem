29fa09cdcdb3d9ec96d3bd04cf8c787d1b7d1c5d
akshata <akshata2018p@gamil.com>
2023-02-07 20:48:53 +0530

    [akshata]Add:EvenorOdd

---
+++ BasicCoreProgram/EvenorOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    public class EvenorOdd
+    {
+        public void EvenOdd()
+        {
+            int rem;
+            Console.WriteLine("Enter the Number is Even or Odd");
+            int num = Convert.ToInt32(Console.ReadLine());
+            rem = num % 2;
+            if (rem == 0)
+                Console.WriteLine("{0} is an Even Number \n", num);
+            else
+                Console.WriteLine("{0} is an Odd Number \n", num);
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -10,7 +10,7 @@ namespace BasicCoreProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1 :FlipCoin \n 2: LeapYear \n 3:PowerOfTwo \n 4:HarmonicNumber \n 5:CalculatingFactor \n 6:QuotientRemainder \n 7:SwappingNumber");
+            Console.WriteLine("1 :FlipCoin \n 2: LeapYear \n 3:PowerOfTwo \n 4:HarmonicNumber \n 5:CalculatingFactor \n 6:QuotientRemainder \n 7:SwappingNumber \n 8:EvenorOdd");
             Console.WriteLine("Enter a Option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
@@ -49,6 +49,11 @@ namespace BasicCoreProgram
                      SwappingNumber swap = new SwappingNumber();
                      swap.swap();
                      break;
+
+                 case 8:
+                    EvenorOdd Evenodd = new EvenorOdd();
+                    Evenodd.EvenOdd();
+                    break;
             }
             Console.ReadLine();
         }
